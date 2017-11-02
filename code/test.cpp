343c71498aa7fc7bb8ae9c11a8732fe36ae59a62
dr_coffee <yhyu.mail@gmail.com>
2017-11-01 21:39:03 -0400

    temp

--- code/test.cpp
+++ code/test.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <string>
 
-//#include <opencv2/core/core.hpp>  
-//#include <opencv2/imgproc/imgproc.hpp>
-//#include <opencv2/highgui/highgui.hpp> 
+#include <opencv2/core/core.hpp>  
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp> 
 
 int main()
 {
