2a5157c106a9bdd551c0a1735881da07829d5d5d
dr_coffee <yhyu.mail@gmail.com>
2017-11-06 20:57:06 -0500

    re-organize the project

--- code/BS_MIC.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class BackgroundMIC
-{
-public:
-	int counter; 
-};
-
---
+++ code/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+#include <opencv2/core/core.hpp>  
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp> 
+
+int main()
+{
+	//Testing
+	std::string path = "E:/YUsStudio/Research__2_Background_Modeling/dataset/1/";
+	char buffer[100];
+	sprintf_s(buffer, "%05d%s", 5, ".png");
+	std::string file = buffer;
+
+	std::cout << path + file << std::endl;
+
+	std::cout << "Hello World!";
+	return 0; 
+	
+	
+}
